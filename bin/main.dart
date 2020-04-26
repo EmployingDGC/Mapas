1a1c780046b0608c34f7952031d9c2e7cf9c5424
EmployingDGC <63275906+EmployingDGC@users.noreply.github.com>
2020-04-26 00:51:16 -0300

    Update main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,25 @@
 void main(List<String> arguments) {
-  print('Hello world!');
+  Map<int, String> ddds = new Map();
+
+  ddds[11] = 'São Paulo';
+  ddds[19] = 'Campinas';
+  ddds[27] = 'Linhares';
+  ddds[28] = 'Alegre';
+
+  Map<String, dynamic> pessoa = new Map();
+
+  pessoa['nome'] = 'Davi';
+  pessoa['idade'] = 23;
+  pessoa['altura'] = 1.75;
+
+  Map<String, InfosPessoa> pessoas = Map();
+  pessoas['Nome1'] = new InfosPessoa(17);
+  pessoas['Nome2'] = new InfosPessoa(16);
+  pessoas['Davi'] = new InfosPessoa(23);
+}
+
+class InfosPessoa {
+  int idade;
+
+  InfosPessoa(this.idade);
 }
